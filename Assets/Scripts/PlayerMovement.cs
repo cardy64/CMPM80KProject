4e6699c25f5e64f7be56f2fb26c360dfea3ae649
Neko <43053729+Neko2n@users.noreply.github.com>
2024-03-06 21:12:39 -0800

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,79 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float maxAcc = 7;
     public float maxSpeed = 7;
-    public float maxTurnSpeed = 3.5f; // in rads per second
-    public float AngularAcc = 30; //rads 
-    public float LinearAcc = 15;
-    float speed = 0;
-    float anuglarVel = 0;
-    float speedCap = 8;
-    float rotation = 0.0f;
-    public float LengthwiseTriction = 0.5f;
-    public float TransverseFriction = 0.9f;
-    public float turnFriction = 0.5f;
-
-    Vector2 vel = new Vector2(0f, 0f);
-    Vector2 movement = new Vector2(0.0f, 0.0f);
-
+    public float acceleration = 1;
+    public float handling = 1;
+    float angularVel = 0;
+    Vector2 linearVel = Vector2.zero;
+    Vector2 input = Vector2.zero;
 
     public Rigidbody2D rb; // Reference to the Rigidbody2D component
-
-    void Start()
-    {
-        Debug.Log("hello");
-    }
 
     void Update()
     {
         // Input gathering
-        movement.x = Input.GetAxisRaw("Horizontal"); // Get horizontal input
-        movement.y = Input.GetAxisRaw("Vertical"); // Get vertical input
-
-        
+        input.x = Input.GetAxisRaw("Horizontal"); // Get horizontal input
+        input.y = 1f + Input.GetAxisRaw("Vertical") * 4; // Get vertical input
+        if (input.y < 0)
+            input.y *= 10;
     }
 
     void FixedUpdate()
     {
-
-
-        //apply friction
-        if (vel.magnitude > 0) {
-            vel -= vel.normalized * LengthwiseTriction * LinearAcc * Time.fixedDeltaTime;
-            anuglarVel -= math.sign(anuglarVel) * AngularAcc * turnFriction * Time.fixedDeltaTime;
-        }
-
         //update velocities
-        anuglarVel += -movement.x * AngularAcc * Time.fixedDeltaTime;
-        vel.x += math.cos(rotation) * movement.y * LinearAcc * Time.fixedDeltaTime;
-        vel.y += math.sin(rotation) * movement.y * LinearAcc * Time.fixedDeltaTime;
+        angularVel += -input.x * handling * Time.fixedDeltaTime / (1 + linearVel.magnitude / 20);
+        Vector2 forwardVector = new Vector2(math.cos(angularVel + math.PI/2), math.sin(angularVel + math.PI/2)).normalized;
+        linearVel += forwardVector * input.y * acceleration * Time.fixedDeltaTime;
 
-        //clamp velocities
-        float cappedTurnSpeed = maxTurnSpeed * vel.magnitude;
-        vel = vel.normalized * math.clamp(vel.magnitude, 0, speedCap);
-        anuglarVel = math.clamp(anuglarVel, -cappedTurnSpeed, cappedTurnSpeed);
+        //re-orient linear velocity
+        float mag = linearVel.magnitude;
+        linearVel = forwardVector * mag;
 
         //ensure true zero
-        if (math.abs(anuglarVel) < 0.1)
-            anuglarVel = 0;
-        
-        if (vel.magnitude < 0.1) {
-            vel.x = 0;
-            vel.y = 0;
+        if (math.abs(angularVel) < 0.001)
+            angularVel = 0;
+        if (math.abs(linearVel.magnitude) < 0.001) {
+            linearVel = Vector2.zero;
         }
 
         //update position
-        rb.MovePosition(rb.position + vel * Time.fixedDeltaTime);
-        rb.SetRotation(rotation * 360 * 7/44);
-        rotation += anuglarVel * Time.fixedDeltaTime;
-
-        // Movement execution
-        //rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + linearVel * Time.fixedDeltaTime);
+        rb.SetRotation(angularVel * 360 * 7/44);
     }
 }
